a4035aa0b7320ecbf4ab2ce51a206d93e6f17fb8
Alexey <ipad2322012@gmail.com>
2023-12-04 10:10:06 +0300

    initial commit

---
+++ task_2/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задайте массив заполненный случайными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = new int[10];
+        Random rnd = new Random();
+        
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rnd.Next(100, 1000);
+        }
+
+        int evenCount = CountEvenNumbers(array);
+
+        Console.WriteLine("Количество чётных чисел в массиве: " + evenCount);
+    }
+
+    static int CountEvenNumbers(int[] array)
+    {
+        int count = 0;
+
+        foreach (int number in array)
+        {
+            if (number % 2 == 0)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
---
+++ task_3/Program.cs
@@ -0,0 +1,34 @@
+﻿//Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };// Создаем массив
+
+        Console.WriteLine("Исходный массив: [" + string.Join(", ", array) + "]");// Выводим исходный массив
+
+        ReverseArray(array); // Переворачиваем массив
+
+        Console.WriteLine("Перевернутый массив: [" + string.Join(", ", array) + "]");// Выводим перевернутый массив
+    }
+
+    // Функция для переворачивания массива
+    static void ReverseArray(int[] arr)
+    {
+        int length = arr.Length;
+
+        for (int i = 0; i < length / 2; i++)
+        {
+            SwapElements(arr, i, length - 1 - i);// Обмениваем элементы массива с противоположными
+        }
+    }
+
+    // Функция для обмена элементов массива
+    static void SwapElements(int[] arr, int index1, int index2)
+    {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+}
